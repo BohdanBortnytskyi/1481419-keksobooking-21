8fccec426b73de6f3b108af20cf8c74b55fe824f
Bohdan <bobo.kiev@gmail.com>
2020-11-30 04:24:08 +0200

    Перемещение в пределах карты

--- js/pin-main.js
+++ js/pin-main.js
@@ -4,6 +4,16 @@
 
 (function () {
 
+  const PIN_MAIN_SIZE = {
+      width: 64,
+      height: 82
+    }
+
+  const PIN_X_MIN = (innerWidth - document.body.clientWidth) / 2 + PIN_MAIN_SIZE.width / 2;
+  const PIN_X_MAX = innerWidth - (innerWidth - document.body.clientWidth) / 2 - PIN_MAIN_SIZE.width / 2;
+  const PIN_Y_MIN = 130;
+  const PIN_Y_MAX = 630;
+
   window.mapPinMain = window.setup.map.querySelector('.map__pin--main');
 
   // Установка значения поля адреса
@@ -15,9 +25,9 @@
     var addressY = window.mapPinMain.style.top.replace('px', '');
 
     if (!window.setup.isPageActive) {
-      addressInput.value = (+addressX + window.pin.PIN_SIZE.width / 2) + ', ' + (+addressY + window.pin.PIN_SIZE.width / 2);
+      addressInput.value = (+addressX + PIN_MAIN_SIZE.width / 2) + ', ' + (+addressY + PIN_MAIN_SIZE.width / 2);
     } else {
-      addressInput.value = (+addressX + window.pin.PIN_SIZE.width / 2) + ', ' + (+addressY + window.pin.PIN_SIZE.height);
+      addressInput.value = (+addressX + PIN_MAIN_SIZE.width / 2) + ', ' + (+addressY + PIN_MAIN_SIZE.height);
     }
   };
 
@@ -58,10 +68,12 @@
           y: moveEvt.clientY
         };
 
-        window.mapPinMain.style.top = (window.mapPinMain.offsetTop - shift.y) + 'px';
-        window.mapPinMain.style.left = (window.mapPinMain.offsetLeft - shift.x) + 'px';
+      if (moveEvt.clientY >= PIN_Y_MIN && moveEvt.clientY <= PIN_Y_MAX && moveEvt.clientX >= PIN_X_MIN && moveEvt.clientX <= PIN_X_MAX) {
+          window.mapPinMain.style.top = (window.mapPinMain.offsetTop - shift.y) + 'px';
+          window.mapPinMain.style.left = (window.mapPinMain.offsetLeft - shift.x) + 'px';
 
-        window.setAddressInputValue();
+          window.setAddressInputValue();
+        }
       };
 
       var onMouseUp = function (upEvt) {
